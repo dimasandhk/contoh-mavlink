c3084e030a103c5b51de6bbde6f3c92c5deb2271
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2024-11-20 10:42:27 +0700

    feat: server template code

--- server/main.py
+++ server/main.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
+from dronekit import connect
 
 app = Flask(__name__)
 
-@app.route('/ping', methods=['GET'])
-def ping():
-  return jsonify({'message': 'pong'})
+vehicle = connect("tcp:127.0.0.1:14550", wait_ready=True)
+vehicle.wait_ready("autopilot_version")
 
-if __name__ == '__main__':
-  app.run(debug=True)
+@app.route("/get_all_params", methods=['GET'])
+def get_all_params():
+    try:
+        params = {param: value for param, value in vehicle.parameters.items()}        
+        return jsonify(params), 402
+    except Exception as e:
+        return jsonify(ok=False)
+    
+app.run(debug=True)
